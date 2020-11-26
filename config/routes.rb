fe7624841cd84f4a887b81550049d0eb3a8e8860
zeradi amine <dasileker@gmail.com>
2020-11-26 14:32:42 +0100

    added style

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-
+  get 'home/index'
 
   resources :posts do
     resources :comments
   end
+
+  root 'home#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
